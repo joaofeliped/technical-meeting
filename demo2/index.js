56933b3d27180fa1a968e3481398d663473b093b
João Felipe <jfpessanha@stefanini.com>
2020-10-23 06:23:27 -0300

    test

---
+++ demo2/index.js
@@ -0,0 +1,65 @@
+const csvtojson = require('csvtojson');
+const { cnpj } = require('cpf-cnpj-validator');
+
+const path = require('path');
+const fs = require('fs');
+
+const { Transform, Writable, pipeline } = require('stream');
+const { promisify } = require('util');
+
+const pipelineAsync = promisify(pipeline);
+
+const mapStream = (elapsedBytes) =>
+  new Transform({
+    objectMode: true,
+    transform: (chunk, encoding, cb) => {
+      elapsedBytes.count += chunk.length;
+      const item = JSON.parse(chunk);
+
+      const data = JSON.stringify({
+        name: item.empresa,
+        cnpj: cnpj.generate(),
+      });
+
+      return cb(null, data);
+    },
+  });
+
+const processDataStream = (finalData) =>
+  new Writable({
+    write: async (chunk, encoding, cb) => {
+      const data = JSON.parse(chunk);
+
+      cb();
+    },
+  });
+
+async function main() {
+  console.log('starting at ', new Date().toISOString());
+  console.time('elapsed time');
+  const elapsedBytes = {
+    count: 0,
+  };
+
+  const surveyFile = {
+    Bucket: 'technical-meeting',
+    Key: 'empresas.csv',
+  };
+
+  const fileStream = s3.getObject(surveyFile).createReadStream();
+
+  // const fileStream = fs.createReadStream(
+  //   path.resolve(__dirname, 'empresas.csv')
+  // );
+
+  await pipelineAsync(
+    fileStream,
+    csvtojson(),
+    mapStream(elapsedBytes),
+    processDataStream(refFinalData)
+  );
+
+  console.timeEnd('elapsed time');
+}
+
+main();
